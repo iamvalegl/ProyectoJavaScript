68ac588de2804f4bbc54cf7f465d83045f7af45f
iamvalegl <valerygomez2007@cesanrafael.org>
2024-11-19 14:25:24 -0400

    Valery7

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+let mensaje="Hola Mundo";
+console.log(mensaje);
+let numeroUno=2;
+let numeroDos=4;
+let numeroTres=10;
+let resultadouno= numeroUno+numeroDos+numeroTres;
+console.log("La suma es ",resultadouno);
+let resultadodos= numeroUno-numeroDos-numeroTres;
+console.log("La resta es ",resultadodos);
+let resultadotres= numeroUno*numeroDos*numeroTres;
+console.log("La multiplicacion es ",resultadotres);
+let resultadocuatro= numeroUno/numeroDos/numeroTres;
+console.log("La division es ",resultadocuatro);
+function sumar(){
+    let numeroUno=2;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno+numeroDos+numeroTres;
+    alert("La suma es: "+ resultado);
+}
+function restar(){
+    let numeroUno=2;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno-numeroDos-numeroTres;
+    alert("La resta es: "+ resultado);
+}
+function multiplicar(){
+    let numeroUno=2;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno*numeroDos*numeroTres;
+    alert("La multiplicacion es: "+ resultado);
+}
+function dividir(){
+    let numeroUno=2;
+    let numeroDos=4;
+    let numeroTres=10;
+    let resultado=numeroUno/numeroDos/numeroTres;
+    alert("La division es: "+ resultado);
+}
